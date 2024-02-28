a029c74f3a46da52fc4e7986a348369687c548fe
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2024-02-28 11:42:53 +0200

    adding category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+
+// category model 
 
 class AdminController extends Controller
 {
@@ -10,4 +15,29 @@ class AdminController extends Controller
     {
         return view('admin.category');
     }
+
+    public function add_category(Request $request)
+    {
+        // Validate incoming request data
+        $request->validate([
+            'name' => 'required|unique:categories,category_name',
+        ]);
+
+        $existingCategory = Category::where('category_name', $request->name)->first();
+
+        if ($existingCategory) {
+            return Redirect::back()->withErrors(['error' => 'Category already exists']);
+            // return redirect()->back()->with(['error' => 'Category already exists']);
+        } else {
+            // Category doesn't exist, proceed with creating the new category
+            $category = new Category();
+            $category->category_name = $request->name;
+            $category->save();
+
+            return redirect()->back()->with(['message' => 'Category added successfully']);
+        }
+
+
+    }
+
 }
--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -4,6 +4,22 @@
 <head>
     <!-- Required meta tags -->
     @include('admin.css')
+    <style>
+        .div_center {
+            text-align: center;
+            margin-top: 50px;
+        }
+
+        .div_center h2 {
+            font-size: 30px;
+            font-weight: 600;
+            padding-bottom: 50px;
+        }
+
+        .input {
+            color: #000;
+        }
+    </style>
 
 </head>
 
@@ -14,6 +30,33 @@
         <!-- partial -->
         @include('admin.header')
         <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                @if(Session::has('message'))
+
+                <div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    {{ Session::get('message') }}
+                </div>
+
+                @endif
+                @if($errors->any('error'))
+                <div class="alert alert-danger">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    {{ $errors->first() }}
+                </div>
+                @endif
+                <div class="div_center">
+                    <h2>Add Category</h2>
+
+                    <form action="{{url('/add_category')}}" method="POST">
+                        @csrf
+                        <input class="input" type="text" name="name" placeholder="add category name">
+                        <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                    </form>
+                </div>
+            </div>
+        </div>
         <!-- container-scroller -->
         <!-- plugins:js -->
         @include('admin.scripts')
